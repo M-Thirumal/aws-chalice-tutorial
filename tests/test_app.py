5e4d087f2c9ee63dfcfd08b703cfa5395b2a1a10
Thirumal <m.thirumal@hotmail.com>
2021-05-11 09:10:58 +0530

    added test

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,17 @@ from chalice.test import Client
 from app import app
 import pytest
 
+
+#Testing AWS Lambda functions
+# client.lambda_ – Used to test Lambda functions by specifying the payload to pass to the Lambda function.
 #@pytest.mark.skip
 def test_foo_function():
     with Client(app, stage_name='dev') as client:
         result = client.lambda_.invoke('foo')
         assert result.payload == {'value': 'TOP LEVEL'}
 
-
+# Testing REST API
+# client.http – Used to test REST APIs.
 def test_pagination():
     with Client(app) as client:
         print("Started testing pagination")
